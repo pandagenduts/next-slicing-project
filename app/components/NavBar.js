2dd31c0a46402714d088aa58c55ac8218bf00a01
Donny Rendi <donny.rendi@yahoo.co.id>
2023-11-24 12:58:35 +0700

    navbar flex

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,17 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function NavBar() {
-  const linkClasses = 'flex-1 py-3 nav-item px-3'
+  const linkClasses = "flex-1 py-3 px-6 lg:px-0 nav-item whitespace-nowrap";
 
   return (
-    <nav className='fixed left-0 bottom-0 w-full flex text-center overflow-auto'>
-      <Link href='/' className={`${linkClasses} nav-active`}>Nightclub Profile</Link>
-      <Link href='/' className={`${linkClasses} nav-active`}>Featured</Link>
-      <Link href='/' className={`${linkClasses} nav-active`}>Beverages</Link>
-      <Link href='/' className={linkClasses}>Table Information</Link>
-      <Link href='/' className={linkClasses}>Operational Hour</Link>
-      <button className='flex-1'>NEXT</button>
+    <nav className="fixed bottom-0 left-0 flex w-full overflow-auto text-center ">
+      <Link href="/" className={`${linkClasses} nav-active`}>
+        Nightclub Profile
+      </Link>
+      <Link href="/" className={`${linkClasses} nav-active`}>
+        Featured
+      </Link>
+      <Link href="/" className={`${linkClasses} nav-active`}>
+        Beverages
+      </Link>
+      <Link href="/" className={linkClasses}>
+        Table Information
+      </Link>
+      <Link href="/" className={linkClasses}>
+        Operational Hour
+      </Link>
+      <button className="flex-1 py-3 px-6 lg:px-0">NEXT</button>
     </nav>
-  )
+  );
 }
